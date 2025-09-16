b54cc497fa43c30ac8dc276cd7704128e44cfa52
SirLJM <spambigbox@gmail.com>
2025-09-17 00:35:18 +0200

    add base sprite class

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
-import pygame
+import contextlib
+with contextlib.redirect_stdout(None):
+    import pygame
 
 from constants import *
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    color = pygame.Color(0,0,0)
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    # color = pygame.Color(0,0,0)
+    dt = 0
+
+    # print("Starting Asteroids!")
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, color)
+
+        screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
